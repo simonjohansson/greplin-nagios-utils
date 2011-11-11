3b4c36983ce0d4034130467b0bd538120b069358
Simon Johansson <870805@gmail.com>
2011-11-11 16:02:30 +0000

    AngryCron should be in its own repo

--- AngryCron/Scripts/test_script.py
+++
@@ -1,8 +0,0 @@
-def test_script(args):
-    #Do something
-    some_number = 10
-    if some_number >= 20:
-        return "CRIT: Some number is %s" % some_number
-    elif some_number >= 10:
-        return "WARN: Some number is %s" % some_number
-    return "OK: some number is %s" % some_number
